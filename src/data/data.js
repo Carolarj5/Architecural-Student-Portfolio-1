28a4a8b0b447b861f4196afc61b1e83ec5546e89
CAROLA RAMIREZ <carola.ramirez1@ucalgary.ca>
2025-01-17 17:41:30 -0700

    I made changes yes whahoo :)

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Carola Ramirez",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
-        linkedin: "https://www.linkedin.com/",
+        email: "Carola.ramirez1@ucalgary.ca",
+        phone: "+9991274887",
         github: "https://www.github.com/",
     }
 }
